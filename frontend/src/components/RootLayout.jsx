932919ff3d537b2212e869e61a3b9cbde8ec0dee
tharun-0909 <tharungandla2006@gmail.com>
2026-05-14 14:18:22 +0530

    Update RootLayout.jsx

--- frontend/src/components/RootLayout.jsx
+++ frontend/src/components/RootLayout.jsx
@@ -5,7 +5,7 @@ function RootLayout() {
   return (
     <div className='bg-slate-400'>
         <Header />
-        <div className='min-h-screen mx-0 sm:mx-5 md:mx-10 lg:mx-20 p-20 bg-linear-to-t from-blue-200 to-purple-300'>
+        <div className='min-h-screen mx-0 sm:mx-5 md:mx-10 lg:mx-20 p-20 bg-slate-400'>
             <Outlet />
         </div>
     </div>
