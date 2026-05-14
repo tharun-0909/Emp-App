92c5c97d173197fcfea3dedbc21bcc106bfb078d
tharun-0909 <tharungandla2006@gmail.com>
2026-05-14 14:09:21 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,10 +2,10 @@ import {NavLink} from 'react-router'
 
 function Header() {
   return (
-    <nav className="flex justify-end gap-11 p-5 bg-violet-200 font-semibold text-violet-600">
-      <NavLink to="" className={({isActive})=>(isActive?"text-violet-800":"hover:text-blue-600")}>Home</NavLink>
-      <NavLink to="create-emp" className={({isActive})=>(isActive?"text-blue-800":"hover:text-violet-700")}>Create Employee</NavLink>
-      <NavLink to="list" className={({isActive})=>(isActive?"text-blue-800":"hover:text-violet-700")}>List of Employees</NavLink>
+    <nav className="flex justify-end gap-11 p-5 bg-amber-200 font-semibold text-violet-600">
+      <NavLink to="" className={({isActive})=>(isActive?"text-violet-800":"hover:text-blue-800")}>Home</NavLink>
+      <NavLink to="create-emp" className={({isActive})=>(isActive?"text-blue-800":"hover:text-violet-800")}>Create Employee</NavLink>
+      <NavLink to="list" className={({isActive})=>(isActive?"text-blue-800":"hover:text-violet-800")}>List of Employees</NavLink>
     </nav>
   )
 }
