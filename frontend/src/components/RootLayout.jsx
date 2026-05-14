2cc5dd26839cbba3c08488617427f1f1a9416479
tharun-0909 <tharungandla2006@gmail.com>
2026-05-14 14:20:18 +0530

    Update RootLayout.jsx

--- frontend/src/components/RootLayout.jsx
+++ frontend/src/components/RootLayout.jsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router'
 
 function RootLayout() {
   return (
-    <div className='bg-slate-400'>
+    <div className='bg-stone-400'>
         <Header />
-        <div className='min-h-screen mx-0 sm:mx-5 md:mx-10 lg:mx-20 p-20 bg-slate-400'>
+        <div className='min-h-screen mx-0 sm:mx-5 md:mx-10 lg:mx-20 p-20'>
             <Outlet />
         </div>
     </div>
